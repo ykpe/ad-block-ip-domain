98b5fa240131267bc6ee1398de18ddc898eae038
JeffLin <tailangjin@gmail.com>
2020-05-28 21:44:41 +0800

    add more host

--- adblock.sh
+++ adblock.sh
@@ -1,4 +1,4 @@
-curl -s -L https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt https://easylist-downloads.adblockplus.org/fanboy-social.txt > adblock.unsorted
+curl -s -L https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts https://gitee.com/privacy-protection-tools/anti-ad/raw/master/domains.txt https://easylist-downloads.adblockplus.org/fanboy-social.txt > adblock.unsorted
 sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
-sed 's/[\|^]//g' < adblock.sorted > adblock.hosts
+sed 's/[\|^]//g' < adblock.sorted > easylistChina-host.txt
 rm adblock.unsorted adblock.sorted
