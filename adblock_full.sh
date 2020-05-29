ec2fd19168d7b925465063f6e6a9ca3b62ea8226
JeffLin <tailangjin@gmail.com>
2020-05-29 21:49:01 +0800

    add full version

---
+++ adblock_full.sh
@@ -0,0 +1,4 @@
+curl -s -L https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts https://gitee.com/privacy-protection-tools/anti-ad/raw/master/domains.txt https://easylist-downloads.adblockplus.org/fanboy-social.txt https://v.firebog.net/hosts/static/w3kbl.txt https://v.firebog.net/hosts/AdguardDNS.txt https://v.firebog.net/hosts/Admiral.txt https://v.firebog.net/hosts/Easylist.txt https://v.firebog.net/hosts/Easyprivacy.txt https://v.firebog.net/hosts/Prigent-Ads.txt https://v.firebog.net/hosts/Prigent-Crypto.txt https://v.firebog.net/hosts/Prigent-Malware.txt https://v.firebog.net/hosts/Shalla-mal.txt > adblock.unsorted
+sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
+sed 's/[\|^]//g' < adblock.sorted > easylistChina-host.txt
+rm adblock.unsorted adblock.sorted
